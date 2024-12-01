bbfe09edc95ab6b899a3a8c2f4324177fe8a48eb
Jonas Grube <jonas.grube@fh-bielefeld.de>
2024-12-01 19:29:32 +0100

    01 Pt2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -4,6 +4,7 @@ use code_timing_macros::time_snippet;
 use const_format::concatcp;
 use itertools::Itertools;
 use regex::Regex;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::isize;
@@ -67,17 +68,38 @@ fn main() -> Result<()> {
     //endregion
 
     //region Part 2
-    // println!("\n=== Part 2 ===");
-    //
-    // fn part2<R: BufRead>(reader: R) -> Result<usize> {
-    //     Ok(0)
-    // }
-    //
-    // assert_eq!(0, part2(BufReader::new(TEST.as_bytes()))?);
-    //
-    // let input_file = BufReader::new(File::open(INPUT_FILE)?);
-    // let result = time_snippet!(part2(input_file)?);
-    // println!("Result = {}", result);
+    println!("\n=== Part 2 ===");
+
+    fn part2<R: BufRead>(reader: R) -> Result<usize> {
+        let (mut left, mut right) = split_lists(reader)?;
+        let mut right_cout_map = HashMap::new();
+        for r_val in &right {
+            match right_cout_map.get(r_val) {
+                Some(count) => {
+                    right_cout_map.insert(r_val, count + 1);
+                }
+                None => {
+                    right_cout_map.insert(r_val, 1);
+                }
+            }
+        }
+
+        let mut similarity_score= 0;
+        for l_val in left{
+            let count = if let Some(c) = right_cout_map.get(&l_val) {
+                *c
+            }else { 0 };
+            similarity_score += l_val * count;
+        }
+
+        Ok(similarity_score as usize)
+    }
+
+    assert_eq!(31, part2(BufReader::new(TEST.as_bytes()))?);
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part2(input_file)?);
+    println!("Result = {}", result);
     //endregion
 
     Ok(())
