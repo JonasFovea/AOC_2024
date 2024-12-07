b566ad207bb535e210a735abb0e3becba7a93362
Jonas Grube <jonas.grube@fh-bielefeld.de>
2024-12-07 09:54:19 +0100

    07 Pt2

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -45,6 +45,49 @@ fn check_slice_add_mul(result: usize, current_result: usize, rest_values: &[usiz
     add_res || mul_res
 }
 
+fn check_slice_add_mul_concat(result: usize, current_result: usize, rest_values: &[usize]) -> bool {
+    if rest_values.len() == 0 {
+        return result == current_result;
+    }
+
+    let next_add = current_result + rest_values[0];
+    let next_mul = if current_result != 0 {
+        current_result * rest_values[0]
+    } else {
+        rest_values[0]
+    };
+    let next_concat: usize = concatenate_integers(current_result, rest_values[0]);
+
+
+    let mut add_res = false;
+    let mut mul_res = false;
+    let mut concat_res = false;
+
+    if next_add <= result {
+        add_res = check_slice_add_mul_concat(result, next_add, &rest_values[1..]);
+    }
+    if next_mul <= result {
+        mul_res = check_slice_add_mul_concat(result, next_mul, &rest_values[1..]);
+    }
+    if next_concat <= result {
+        concat_res = check_slice_add_mul_concat(result, next_concat, &rest_values[1..]);
+    }
+
+    add_res || mul_res || concat_res
+}
+
+fn concatenate_integers(a: usize, b: usize) -> usize {
+    let mut b_copy = b;
+    let mut digits = 0;
+
+    while b_copy > 0 {
+        b_copy /= 10;
+        digits += 1;
+    }
+
+    a * 10_usize.pow(digits) + b
+}
+
 fn read_calibrations<R: BufRead>(reader: R) -> Vec<(usize, Vec<usize>)>{
     reader.lines()
         .map(|x| {
@@ -87,17 +130,24 @@ fn main() -> Result<()> {
     //endregion
 
     //region Part 2
-    // println!("\n=== Part 2 ===");
-    //
-    // fn part2<R: BufRead>(reader: R) -> Result<usize> {
-    //     Ok(0)
-    // }
-    //
-    // assert_eq!(0, part2(BufReader::new(TEST.as_bytes()))?);
-    //
-    // let input_file = BufReader::new(File::open(INPUT_FILE)?);
-    // let result = time_snippet!(part2(input_file)?);
-    // println!("Result = {}", result);
+    println!("\n=== Part 2 ===");
+
+    fn part2<R: BufRead>(reader: R) -> Result<usize> {
+        let calibrations: Vec<(usize, Vec<usize>)> = read_calibrations(reader);
+
+        let answer = calibrations
+            .iter()
+            .filter(|(result, values)| check_slice_add_mul_concat(*result, 0, values))
+            .map(|(result, _)| *result).sum();
+
+        Ok(answer)
+    }
+
+    assert_eq!(11387, part2(BufReader::new(TEST.as_bytes()))?);
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part2(input_file)?);
+    println!("Result = {}", result);
     //endregion
 
     Ok(())
